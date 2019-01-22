c9457addaee6bc21b3fbe006dc28fed60a233087
congbing <congbing@everybodycreate.com>
2019-01-22 15:42:36 +0800

    自动生成domain和mapper

---
+++ src/main/java/com/cb/itoken/service/admin/mapper/TbSysUserMapper.java
@@ -0,0 +1,7 @@
+package com.cb.itoken.service.admin.mapper;
+
+import com.cb.itoken.service.admin.domain.TbSysUser;
+import tk.mybatis.mapper.MyMapper;
+
+public interface TbSysUserMapper extends MyMapper<TbSysUser> {
+}

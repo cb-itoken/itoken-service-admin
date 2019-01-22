1637ed21d6ee8501caf9e484e47044b96524b435
congbing <congbing@everybodycreate.com>
2019-01-22 14:42:52 +0800

    自动生成domain和mapper

--- src/main/java/com/cb/itoken/service/admin/ServiceAdminApplication.java
+++ src/main/java/com/cb/itoken/service/admin/ServiceAdminApplication.java
@@ -3,9 +3,11 @@ package com.cb.itoken.service.admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @EnableEurekaClient
+@MapperScan(basePackages = "com.cb.itoken.service.admin.mapper")
 public class ServiceAdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceAdminApplication.class, args);

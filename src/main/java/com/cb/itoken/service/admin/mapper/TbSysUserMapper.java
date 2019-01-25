ae1f204981d21140b96ae8914b3df7be840b0f62
congbing <congbing@everybodycreate.com>
2019-01-25 17:44:23 +0800

    提取统一的domain、dao、mapper.xml

--- src/main/java/com/cb/itoken/service/admin/mapper/TbSysUserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.cb.itoken.service.admin.mapper;
-
-import com.cb.itoken.common.domain.TbSysUser;
-import tk.mybatis.mapper.MyMapper;
-
-public interface TbSysUserMapper extends MyMapper<TbSysUser> {
-}
--- src/main/java/com/cb/itoken/service/admin/service/AdminService.java
+++ src/main/java/com/cb/itoken/service/admin/service/AdminService.java
@@ -4,17 +4,4 @@ package com.cb.itoken.service.admin.service;
 import com.cb.itoken.common.domain.TbSysUser;
 
 public interface AdminService {
-    /**
-     * 注册
-     * @param tbSysUser
-     */
-    public void register(TbSysUser tbSysUser);
-
-    /**
-     * 登录
-     * @param loginCode 登录账号
-     * @param plantPassword 明文密码
-     * @return
-     */
-    public TbSysUser login(String loginCode, String plantPassword);
 }
